e09875b73d8e666fe8982c3d5d79110a383c18c5
sujin22 <aoghksj@naver.com>
2020-11-14 22:32:46 +0900

    Sidebar layout만

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import logo from './sw_logo.png';
 import './App.css';
 import {HashRouter, Route} from 'react-router-dom';
 import Login from './routes/Login';
+import Main from './routes/Main';
 
 function App(){
 
   return(
     <HashRouter>
-      <Route path="/" exact="true" component={Login}/>
+      <Route path="/" exact="true" component={Main}/>
     </HashRouter>
   );
 }
--- src/routes/Main.js
+++ src/routes/Main.js
@@ -2,14 +2,63 @@ import React from 'react';
 import './Main.css';
 
 class Main extends React.Component{
+    state ={
+        isSidebarExpanded: true
+    };
+
+    sidebarExpanded = () => (
+        <div className="sidebar">
+            <span
+            role="presentation"
+            onClick={() => this.setState({isSidebarExpanded: false})}
+            >
+                사이드바 접기
+            </span>
+
+            <ul>
+                <li>Menu item 1</li>
+                <li>Menu item 2</li>
+                <li>Menu item 3</li>
+            </ul>
+        </div>
+    );
+
+    sidebarCollapsed = () => (
+        <div className="sidebar collapsed">
+                    <span
+                    role="presentation"
+                    onClick={() =>
+                        this.setState({
+                            isSidebarExpanded:true
+                        })
+                        }
+                    >
+                        펼치기
+                    </span>
+
+                    <ul>
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
+                    </ul>
+                </div>
+    );
+
     render(){
+        const { isSidebarExpanded } = this.state;
         return(
+            
             <div className="Main">
-                <div className="sidebar"></div>
-                <div className="content"></div>
+                
+                {isSidebarExpanded && this.sidebarExpanded()}
+                {isSidebarExpanded || this.sidebarCollapsed()}
+                
+                <div className="content">
+                    <p> Content </p>
+                </div>
             </div>
         );
     }
 }
 
-export default React;
+export default Main;
