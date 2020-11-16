4d55ee88514b5472581b7b4b8059dc0f752a60ea
sujin22 <aoghksj@naver.com>
2020-11-17 01:07:59 +0900

    Sidebar - 디자인 추가 전, 애니메이션 추가 전

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,54 +1,58 @@
 import React from 'react';
 import {Component} from 'react';
 import './Sidebar.css'
-import logo from '../sw_logo.png';
+import icon_expanded from '../icon_expanded.png';
+import icon_collapsed from '../icon_collapsed.png';
 
 class Sidebar extends Component{
     state ={
-        isSidebarExpanded: true
+        isSidebarExpanded: false
     };
 
-    sidebarExpanded = () => (
+    sidebarCollapsed = () => (
         <div className="sidebar">
+                    <span
+                    role="presentation"
+                    onClick={() => this.setState({isSidebarExpanded:true})}
+                    >
+                         <img src={icon_expanded} className="icon_expanded" alt="expanded" />
+                    </span>
+                </div>
+    );
+    
+    sidebarExpanded = () => (
+        <div className="sidebar expanded">
             <span
             role="presentation"
             onClick={() => this.setState({isSidebarExpanded: false})}
             >
-                사이드바 접기
+                <img src={icon_collapsed} className="icon_collapsed" alt="collapsed" />
             </span>
 
-            <div className="sidebuttons">
-                <button onclick="alert('마이페이지')">마이페이지</button>
-                <button onclick="alert('로그아웃')">로그아웃</button>
+            <div className="sidebar_contents">
+                <div className="user_info">
+                    16011144님, 안녕하세요.
+                </div>
+                <div className="buttons">
+                    <button onclick="alert('로그아웃')">로그아웃</button>
+                </div>            
+
+                <ul>
+                    <li>내 사물함 정보</li>
+                    <li>사물함 배정</li>
+                    <li>회원가입신청관리</li>
+                </ul>
+
             </div>
-
-            <ul>
-                <li>사물함 배정</li>
-                <li>회원가입신청관리</li>
-            </ul>
-
-            
 
         </div>
     );
 
-    sidebarCollapsed = () => (
-        <div className="sidebar collapsed">
-                    <span
-                    role="presentation"
-                    onClick={() =>
-                        this.setState({
-                            isSidebarExpanded:true
-                        })
-                        }
-                    >
-                         <img src={logo} className="Login-logo" alt="logo" />
-                    </span>
-                </div>
-    );
+    
 
     render(){
         const { isSidebarExpanded } = this.state;
+    
         return(
             <div>
                 {isSidebarExpanded && this.sidebarExpanded()}
