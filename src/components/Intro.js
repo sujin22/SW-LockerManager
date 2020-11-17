4a844827561e0c3308ec72a0e45b41c2b648f3bb
sujin22 <aoghksj@naver.com>
2020-11-18 00:22:43 +0900

    Main

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import {Component} from 'react';
 import './Intro.css';
+import * as goScroll from '../routes/Main.js';
 
 class Intro extends Component{
 
+  goScroll = () =>{
+    console.log("눌렸습니다.");
+     }
     render(){
       return(
         <div className="intro">
-            <div className="intro_wraper">
+            <div className="intro_wrapper">
                 <p className="intro_title">
                 Sejong Software<br/>Locker
                 </p>
                 <p className="intro_content">
                 소프트웨어학과 학우 여러분을 위한 사물함이 준비되어있습니다.
                 </p>
-                <button className="intro_btn">
+                <button className="intro_btn"
+                        onClick={this.goScroll}
+                >
                     Click
                 </button>
             </div>
--- src/routes/Main.js
+++ src/routes/Main.js
@@ -1,11 +1,14 @@
-import {React, Component} from 'react';
+import React from 'react';
+import {Component} from 'react';
 import './Main.css';
 import Sidebar from '../components/Sidebar';
 import Intro from '../components/Intro';
 import Minimap from '../components/Minimap';
 
 class Main extends Component{
+
     render(){
+
         return(
             
             <div className="Main">
@@ -15,19 +18,27 @@ class Main extends Component{
 
                 <div className="content">
                     <Intro />
-                    <div className="assignment">
-                        <p className="assignment_title">
-                            사물함 신청
-                        </p>
+                    <div className="assignment" ref={this.scrollRef}>
+                        <div className="assignment_wrapper">
+                            <p className="assignment_title">
+                                사물함 신청
+                            </p>
 
-                        <p className="minimap_title">
-                            사물함을 선택하세요
-                        </p>
-                        <Minimap/>
-                        
-                        <p className="locker_title">
-                            이용하고 싶은 칸을 선택하세요
-                        </p>
+                            <div className="assignment_minimap">
+                                <p className="minimap_title">
+                                    사물함을 선택하세요
+                                </p>
+                                <Minimap/>
+                            </div>
+                            
+                            
+                            <div className="assignment_locker">
+                                <p className="locker_title">
+                                    이용하고 싶은 칸을 선택하세요
+                                </p>
+                                
+                            </div>
+                        </div>
                     </div>
                     
                 </div>
