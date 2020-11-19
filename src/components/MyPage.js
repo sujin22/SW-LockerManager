6a40a5d9659af33700187b41236ce6268cac8d19
sujin22 <aoghksj@naver.com>
2020-11-19 22:37:25 +0900

    MyPage 데이터 입력 완료

--- src/components/MyPage.js
+++ src/components/MyPage.js
@@ -16,27 +16,52 @@ function MyPage(){
         }
     }, [])
 
-    return (
-        <div className="Mypage">
-            <div className="sidebar">
-                { Object.keys(user).length > 0  && <Sidebar admin={auth().isAdmin()} user={user} /> }
+    if(user.locker !== undefined){
+        return (
+            <div className="Mypage">
+                <div className="sidebar">
+                    { Object.keys(user).length > 0  && <Sidebar admin={auth().isAdmin()} user={user} /> }
+                </div>
+                
+                <div className="mypage_container">
+                    <table>
+                        <td className="mypage_img">
+                            <img src={img_mypage}/>
+                        </td>
+                        <td className="mypage_lockerinfo">
+    
+                            <span className="user_info">{user.name}</span>님의 사물함은,<br/><br/>
+                            대양AI센터 B1 사물함 복도의 <span className="user_info">{user.locker.area} - {user.locker.number}</span> 입니다.
+                            
+                        </td>
+                    </table>
+                </div>
             </div>
-            
-            <div className="mypage_container">
-                <table>
-                    <td className="mypage_img">
-                        <img src={img_mypage}/>
-                    </td>
-                    <td className="mypage_lockerinfo">
-
-                        <span className="user_info">16011144</span>님의 사물함은,<br/><br/>
-                        대양AI센터 B1 사물함 복도의 <span className="user_info">A - 24</span> 입니다.
-
-                    </td>
-                </table>
+        );
+    }else{
+        return (
+            <div className="Mypage">
+                <div className="sidebar">
+                    { Object.keys(user).length > 0  && <Sidebar admin={auth().isAdmin()} user={user} /> }
+                </div>
+                
+                <div className="mypage_container">
+                    <table>
+                        <td className="mypage_img">
+                            <img src={img_mypage}/>
+                        </td>
+                        <td className="mypage_lockerinfo">
+    
+                            <span className="user_info">{user.name}</span>님은,<br/><br/>
+                            배정받은 사물함이 없습니다.
+                            
+                        </td>
+                    </table>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+    
 }
 
 export default MyPage;
