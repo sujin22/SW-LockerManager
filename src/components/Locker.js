78e317ef21e775416ab92d60c9c89ad77a7fb793
SnoopyKim <snoopykim0@gmail.com>
2020-11-20 15:39:31 +0900

    사용 가능 유무 체크 기능 완료 & 사용자 사물함은 별도 색 지정

--- src/components/Locker.js
+++ src/components/Locker.js
@@ -2,11 +2,12 @@ import React, { useState, useEffect } from 'react';
 import Proptypes from 'prop-types';
 import './Locker.css';
 import { LockerDB } from '../server/firebase';
+import auth from '../server/auth';
 
 const Locker = ({ data , handler}) => {
     const { area, number, able, user } = data;
 
-    //available
+    //available (true:white / false:gray)
     if(user===undefined){
         return(
             <div style={{float: 'left', width: 50, height: 50, margin: 2, color: 'black', backgroundColor: able? "white" : "gray",transition: "background-color 1s", border:"1px solid gray"}}
@@ -16,10 +17,10 @@ const Locker = ({ data , handler}) => {
         );
     }
 
-    //occupied(blue) / broken(gray)
+    //is it mine? (true:blueviolet / false:blue)
     else{
         return(
-            <div style={{float: 'left', width: 50, height: 50, margin: 2, color: 'white', backgroundColor: able? "blue" : "gray", transition: "background-color 1s", border:"1px solid gray"}}
+            <div style={{float: 'left', width: 50, height: 50, margin: 2, color: 'white', backgroundColor: user.id === auth().getCurrentUser().id? "blueviolet" : "blue", transition: "background-color 1s", border:"1px solid gray"}}
             onClick={handler}>
               {area}<br />{number}
             </div>
